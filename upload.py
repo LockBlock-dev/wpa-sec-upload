052d2f5aaa9fa8bc3eba81c0c5ab41a92f69d176
SomeRandom-Dev <46102926+SomeRandom-Dev@users.noreply.github.com>
2022-06-01 06:20:30 +0000

    clean the code

--- upload.py
+++ upload.py
@@ -1,19 +1,39 @@
-import requests
-from os import listdir, mkdir, path
+import requests, os
+from os import listdir, mkdir
 from subprocess import run, DEVNULL
 from json import load
 
-try:
-    mkdir("./hashes")
-except:
-    pass
+if not os.path.isdir("./hashes"):
+    try:
+        os.rename("./hashes", "./hashes.old")
+        print('Renamed your hashes file to "hashes.old"!')
+    except:
+        pass
+    try:
+        mkdir("./hashes")
+    except:
+        print(
+            'Failed to make hashes directory! Make a directory called "hashes" and restart the program.'
+        )
+        exit()
+
+if not os.path.isdir("./handshakes"):
+    print(
+        'Failed to find handshakes! Create a directory called "handshakes", place your handshakes inside and restart the program.'
+    )
+    exit()
 
 with open("config.json", "r") as f:
     config = load(f)
     f.close()
 
-for i in range(len(config["whitelist"])):
-    config["whitelist"][i] = config["whitelist"][i].lower()
+new_config = {"whitelist": []}
+
+for whitelisted_ssid in config["whitelist"]:
+    new_config.append(whitelisted_ssid.lower())
+
+config = new_config
+new_config = None
 
 handshakes = listdir("./handshakes/")
 hashes = listdir("./hashes/")
@@ -22,15 +42,17 @@ flag = False
 msg = ""
 uploaded = 0
 
-for i in range(size):
-    ssid = handshakes[i].lower().split("_")[0]
+i = 0
+
+for handshake in handshakes:
+    ssid = handshake.lower().split("_")[0]
     if ssid in config["whitelist"]:
-        msg = "Handshake is in the whitelist"
-    elif handshakes[i].endswith(".pcap"):
+        msg = "\033[93mHandshake is in the whitelist!\033[0m"
+    elif handshake.endswith(".pcap"):
         run(
             [
                 "hcxpcapngtool",
-                f"./handshakes/{handshakes[i]}",
+                f"./handshakes/{handshake}",
                 "-o",
                 f"./hashes/{ssid}.hc22000",
             ],
@@ -39,7 +61,7 @@ for i in range(size):
             stdout=DEVNULL,
         )
 
-        if path.exists(f"./hashes/{ssid}.hc22000"):
+        if os.path.exists(f"./hashes/{ssid}.hc22000"):
             f = open(f"./hashes/{ssid}.hc22000", "r")
             hash = f.read()
             f.close()
@@ -48,20 +70,24 @@ for i in range(size):
             old_hashes = old.read().split("\n")
             old.close()
 
-            c = 0
-
+            c = False
+            """ old funny code
             for e in hash.split("\n"):
                 if len(e) > 0 and e in old_hashes:
                     c += 1
+            """
+            for currenthash in hash.split("\n"):
+                if (len(currenthash) != 0) and (currenthash in old_hashes):
+                    c = True
 
-            if c == 0:
+            if c == False:
                 old = open("./old.txt", "a")
                 old.write(hash)
                 old.close()
 
-                handshake = open(f"./handshakes/{handshakes[i]}", "rb")
+                handshake = open(f"./handshakes/{handshake}", "rb")
                 payload = {"file": handshake}
-                headers = {"user-agent": "test"}
+                headers = {"user-agent": "LockBlock-Dev/wpa-sec-upload (1.0.0)"}
                 cookies = {"key": config["api_key"]}
                 r = requests.post(
                     f'{config["api_url"]}?submit',
@@ -72,23 +98,23 @@ for i in range(size):
                 handshake.close()
 
                 if r.text == "Not a valid capture file. We support pcap and pcapng.":
-                    msg = "Not a valid capture file"
+                    msg = "\033[91mInvalid capture file!\033[0m"
                 elif r.text == "No valid handshakes/PMKIDs found in submitted file.":
-                    msg = "No valid handshakes/PMKIDs found"
+                    msg = "\033[91mNo valid handshakes/PMKIDs found!\033[0m"
                 else:
                     print(r.text)
-                    msg = "Handshake uploaded"
+                    msg = "\033[92mHandshake uploaded!\033[0m"
                     uploaded += 1
             else:
-                msg = "Handshake already uploaded"
+                msg = "\033[93mHandshake already uploaded!\033[0m"
         else:
-            msg = "Not a valid capture file"
+            msg = "\033[91mInvalid capture file!\033[0m"
     else:
-        msg = "Not a handshake file"
+        msg = "\033[91mNot a handshake file!\033[0m"
 
     flag = False
-
-    print(f"Handshake {i+1}/{size}: {handshakes[i]} => {msg}")
-print(f"\nUploaded {uploaded} handshakes on {size} handshakes")
+    i = i + 1
+    print(f"Handshake {i}/{size}: {handshake} => {msg}")
+print(f"\nUploaded {uploaded} handshakes of {size} handshakes!")
 
 run(["rm", "-r", "./hashes"])
